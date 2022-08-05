77abb67662b1d30ea50579a9c241ac1a79b29f2d
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-08-05 14:19:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee ={ 
+    name:"Cynthia",
+    streetAdress:"Nairobi",
+   
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+        const newEmployee = {...employee}
+        newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newEmployee = {...employee}
+
+    delete newEmployee.name
+    delete newEmployee.streetAdress
+
+  return newEmployee;
+
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee.name
+    delete employee.streetAdress
+    return employee;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee[key]=value;
+return employee;
+}   
